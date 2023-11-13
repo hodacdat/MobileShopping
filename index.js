51f2f6399bae74e28d7d5e917e04d2042bdce4e3
hodacdat <68152269+hodacdat@users.noreply.github.com>
2023-11-13 20:08:47 +0700

    done home page

--- index.js
+++ index.js
@@ -36,4 +36,38 @@ $(document).ready(function () {
     var filterValue = $(this).attr("data-filter");
     $grid.isotope({ filter: filterValue });
   });
+
+  //new-phones carousel
+
+  $("#new-phones .owl-carousel").owlCarousel({
+    loop: true,
+    nav: false,
+    dots: true,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      600: {
+        items: 3,
+      },
+      1000: {
+        items: 5,
+      },
+    },
+  });
+
+  //blog owl carousel
+  $("#blogs .owl-carousel").owlCarousel({
+    loop: true,
+    nav: false,
+    dots: true,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      600: {
+        items: 3,
+      },
+    },
+  });
 });
